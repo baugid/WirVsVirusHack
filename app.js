9b05d7c83d36a888a4467d42736bed80b568171d
baugid <fun-man2000@web.de>
2020-03-22 15:02:39 +0100

    improved app.js and removed duplicated id column

--- app.js
+++ app.js
@@ -2,31 +2,28 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const Datastore = require('nedb')
-const UIDGenerator = require('uid-generator')
 const uniqueSlug = require('unique-slug')
+const fs = require('fs')
 
+//global constants
 const allowedFileTypes = new Map([['image/png', '.png'], ['application/pdf', '.pdf'], ['image/bmp', '.bmp'], ['image/jpeg', '.jpg']])
+const PORT = 3000
 
 //setup database
 const database = new Datastore("data.db")
 database.loadDatabase()
-database.ensureIndex({ fieldName: 'id', unique: true })
 
-//init UID stuff
-const uidgen = new UIDGenerator(64, UIDGenerator.BASE58)
+//ensure userfiles directory exists
+if (!fs.existsSync('userfiles'))
+    fs.mkdirSync('userfiles');
 
-//Inserts data with an unique Identifier in the database
-function insertData(data, callback) {
-    data.id = uidgen.generateSync()
-    database.insert(data, (err, doc) => {
-        if (err) {
-            if (errorType == 'uniqueViolated')
-                return insertData(data, callback)
-            else
-                return callback(err, doc)
-        }
-        return callback(err, doc)
-    })
+function generateFilename(extension) {
+    let filename = ''
+    do {
+        filename = uniqueSlug() + extension
+    } while (fs.existsSync('userfiles/' + filename))
+
+    return filename
 }
 
 //setup server
@@ -36,37 +33,45 @@ app.use("/getFile", express.static("userfiles"))
 app.use(fileUpload())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.listen(3000)
+app.listen(PORT)
 
 //read form information
 app.post('/readForm', (request, response) => {
     if (!request.body.id)
         return response.status(400).send("Invalid request.")
 
-    database.findOne({ id: request.body.id }, (err, data) => {
-        if (err)
-            return response.status(400).send("Id doesn't exist.")
-
-        delete data._id //internal id should not be exposed
+    database.findOne({ _id: request.body.id }, (err, data) => {
+        if (err || !data) {
+            console.log(err)
+            return response.status(404).send("Id doesn't exist.")
+        }
+        //rename id
+        data.id = data._id
+        delete data._id
         return response.json(data)
     })
 })
 
-//submit File
+//submit file
 app.post('/submitFile', (request, response) => {
     if (!request.files || Object.keys(request.files).length === 0)
-        return response.status(400).send('No files were uploaded.')
+        return response.status(400).send('No files were sent.')
 
     const f = request.files.file
     const filetype = f.mimetype
 
     if (allowedFileTypes.has(filetype)) {
-        const filename = uniqueSlug() + allowedFileTypes.get(filetype)
-
-        f.mv('userfiles/' + filename)
-        return response.json({ fileID: filename, type: filetype })
-    } else
-        return response.status(400).send('Illegal file type.')
+        filename = generateFilename(allowedFileTypes.get(filetype))
+        //try to copy file
+        try {
+            f.mv('userfiles/' + filename)
+            return response.json({ fileID: filename, type: filetype })
+        } catch (error) {
+            console.log(error)
+            return response.status(500).send("Can't store file.")
+        }
+    }
+    return response.status(400).send('Illegal file type.')
 })
 
 //stores the form in the database
@@ -74,9 +79,11 @@ app.post('/submitForm', (request, response) => {
     if (!request.is('json'))
         return response.status(400).send("json expected.")
 
-    insertData(request.body, (err, data) => {
-        if (err)
+    database.insert(request.body, (err, data) => {
+        if (err) {
+            console.log(err)
             return response.status(500).send("Unable to store data.")
-        return response.json({ id: data.id })
+        }
+        return response.json({ id: data._id })
     })
 })
